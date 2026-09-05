d36332fe2d155e0b2fe2a803077aa5bb843fcafd
zpt0.dev <zpt0.dev@outlook.com>
2026-09-05 14:56:01 +0200

    chore: bump version to 1.2.0

--- constants.ts
+++ constants.ts
@@ -1,4 +1,4 @@
-export const PLUGIN_VERSION = "1.1.2";
+export const PLUGIN_VERSION = "1.2.0";
 export const GITHUB_REPO = "zpt0/Clonecord";
 export const UPDATE_CHECK_URL = GITHUB_REPO
     ? `https://api.github.com/repos/${GITHUB_REPO}/releases/latest`
